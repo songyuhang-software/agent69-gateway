41bb8900cee8081091b133f6a8a849b676574cb8
songyuhang013 <songyuhang013@ke.com>
2026-01-14 20:30:33 +0800

    测试网关

--- server.js
+++ server.js
@@ -6,20 +6,32 @@ const app = express();
 // 转发  → 用户服务
 app.use(['/api/users', '/api/personas', '/api/wechat/users'], createProxyMiddleware({
     target: 'http://agent69-userservice.zeabur.internal',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/users': '/api/users', // 保持原始路径
+        '^/api/personas': '/api/personas',
+        '^/api/wechat/users': '/api/wechat/users'
+    }
 }));
 
 
 // 转发  → 文件服务
 app.use(['/api/files', '/api/image'], createProxyMiddleware({
-    target: 'http://file-platform.zeabur.internal',
-    changeOrigin: true
+    target: 'https://file-platform.zeabur.internal',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/files': '/api/files', // 保持原始路径
+        '^/api/image': '/api/image'
+    }
 }));
 
 // 转发  → llm服务
 app.use('/api/agent', createProxyMiddleware({
     target: 'http://agent69-llm-service.zeabur.internal:8000',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/agent': '/api/agent' // 保持原始路径
+    }
 }));
 
 
